f1f695fa7f5227c8efd0f0e7da593035cdc8a5eb
ben <ben.egg.gov@gmail.com>
2022-08-21 11:59:51 +0300

    centered the page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,18 @@
+import { Container } from '@mui/material'
 import Head from 'next/head'
 import Calculator from '../components/calculator'
 
-export default function Home() { 
+export default function Home() {
   return (
-    <div >
-      
-
-      <Calculator/>
-    </div>
+    <div style={{ "height": "500px" }}>
+      <Container style={{
+        "height": "100%",
+        "display": "flex",
+        "justifyContent": "center",
+        "alignItems": "center",
+      }}>
+        <Calculator />
+      </Container>
+    </div >
   )
 }
